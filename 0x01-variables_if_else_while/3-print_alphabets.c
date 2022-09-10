cf535b091366a648b9535ede64f87986713b8979
Oyinloye Temiloluwa <adelolu6@gmail.com>
2022-09-09 20:45:52 -0700

    task 2 and 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	char alpha;
+	char Alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for  (Alpha = 'A'; Alpha <= 'Z'; Alpa++)
+		putchar(Alpha);
+
+	putchar('\n');
+	return (0);
+}
