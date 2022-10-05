07a9e9f8c9cc551d9c05425313b9520365648a1e
Oyinloye Temiloluwa <adelolu6@gmail.com>
2022-10-05 16:49:50 -0700

    task 2 to task 5

---
+++ 0x0A-argc_argv/2-argc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: aguement length
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that adds positive numbers
+ * @argc: argument length
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, i, reusult = 0;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
